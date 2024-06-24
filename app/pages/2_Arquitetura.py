b3215184ca4c54a77e9d8ca993036f0127017a86
ewerthonk <kutz.ewerthon@gmail.com>
2024-06-24 15:47:35 -0300

    updating image size

--- app/pages/2_Arquitetura.py
+++ app/pages/2_Arquitetura.py
@@ -11,7 +11,12 @@ RESOURCES_DIR = Path(__file__).parent.parent.parent.joinpath("resources")
 st.set_page_config(
     page_title="Teste | Case Acerto",
     page_icon=str(RESOURCES_DIR.joinpath("logo_1.png")),
-    layout="wide",
 )
 st.title("Arquitetura")
-st.image(str(RESOURCES_DIR.joinpath("diagram.png")), width=1100)
+with st.sidebar:
+    "[Planejamento](https://docs.google.com/document/d/1KjYd8HR94FH1ZMoEWxI6SCZ_lVyIvGIF_7jPZDHtk9c/edit?usp=sharing)"
+    "[Apresentação](https://docs.google.com/presentation/d/1SwAivlGW-JY7K1KCBE56eMsO9HwCef_oI5Y4f8OcEds/edit?usp=sharing)"
+    "[Repositório](https://github.com/ewerthonk/ai-assistant-debt)"
+
+# Page content
+st.image(str(RESOURCES_DIR.joinpath("diagram.png")))
